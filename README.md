828c9e45c86f9746e36f67fafd8308f6aba0d0c8
chang park <chgpark@keti.re.kr>
2022-11-09 20:05:14 +0900

    simple explanation is written in README.md, discord_webhook_parcer.sh is changed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # discord_webhook_parcer
+
+1. set env variable WEBHOOK_URL
+2. modify discord_webhook_parcer.sh
+3. run discord_webhook_parcer.sh
--- discord_webhook_parcer.sh
+++ discord_webhook_parcer.sh
@@ -1,1 +1,1 @@
-python discord_webhook_parcer.py --host 0.0.0.0 --port 80 --webhook_url https://discord.com/api/webhooks/1039811311946694677/bhyyTnd2KP3-5gktwrzdsBtCJI3dzwO83zk9P6fowrhcJjyoHJMGvsbUTJO-272fNoY7
+python discord_webhook_parcer.py --host 0.0.0.0 --port 80 --webhook_url $WEBHOOK_URL
